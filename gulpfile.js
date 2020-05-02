90aa963d4ca76ff16d6e80e590797d0bec0340fe
unknown <danieleidenilsen@hotmail.com>
2020-05-02 13:16:58 +0200

    update

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const minCSS = require('gulp-csso');
+const { src, dest } = require('gulp');
+const imagemin = require('gulp-imagemin')
+const browserSync = require('browser-sync').create();
+
+function MinMyCSS() { 
+    return src('dist/css/**/*.css').pipe(minCSS()).pipe(dest('dist')).pipe(browserSync.stream())
+}
+function images() { 
+    return src('image/*').pipe(imagemin()).pipe(dest('dist/image')).pipe(browserSync.stream())
+}
+
+function watch()  { 
+    browserSync.init({
+        server: {
+          baseDir: './dist'
+        }
+    });
+    gulp.watch('./dist/css/**/*.css', MinMyCSS);
+    gulp.watch('dist/*.html').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
+exports.images = images;
